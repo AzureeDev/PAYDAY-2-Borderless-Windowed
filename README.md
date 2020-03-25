498749ac1287f1f606ab334d1ab4b813105ee3b1
Sora <timertimer5@gmail.com>
2020-03-25 15:51:33 +0100

    1.0

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-PAYDAY 2 Borderless Windowed
+# PAYDAY 2 Borderless Windowed SBLT Native Plugin
+
+Transforms PAYDAY 2's window into borderless windowed.
+
+## Licence
+
+This project is licenscd under the GNU General Public Licence, version 3. A copy of this
+licence is included in the form of `LICENSE.txt`.
---
+++ src/main.cpp
@@ -0,0 +1,129 @@
+// Include SuperBLT
+#include <superblt_flat.h>
+
+// Include windows.h for whnd
+#include <Windows.h>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+static int timer = 0;		// Delay the initialization by a little bit.
+static int timerTarget = 90;	// Assuming 30 frames per second here, this is how it is when loading the game. We delay it by 6 seconds.
+static bool attemptTransform = false;
+
+enum class TransformErrorCode
+{
+	SUCCESS,
+	ERR_APP_FULLSCREEN,
+	ERR_UNKNOWN
+};
+
+//==============================================
+//	Returns true if the hwnd is fullscreen
+//	https://stackoverflow.com/questions/7009080/detecting-full-screen-mode-in-windows
+//==============================================
+bool IsFullscreen(HWND& window)
+{
+	RECT a, b;
+	GetWindowRect(window, &a);
+	GetWindowRect(GetDesktopWindow(), &b);
+
+	return (a.left == b.left &&
+		a.top == b.top &&
+		a.right == b.right &&
+		a.bottom == b.bottom);
+}
+
+TransformErrorCode TransformWindow()
+{
+	// Get the window handle
+	wstring windowName = L"PAYDAY 2";
+	HWND wHandle = FindWindowW(NULL, windowName.c_str());
+
+	// at this point we have both the hwnd and pid
+	// see if that shit is not fullscreen, to only mess with the hwnd in that specific case
+	if (!IsFullscreen(wHandle))
+	{
+		RECT desktop;
+
+		const HWND hDesktop = GetDesktopWindow();
+		GetWindowRect(hDesktop, &desktop);
+
+		int WINDOW_WIDTH = desktop.right;
+		int WINDOW_HEIGHT = desktop.bottom;
+
+		SetMenu(wHandle, NULL);
+
+		LONG lStyle = GetWindowLong(wHandle, GWL_STYLE);
+		lStyle &= ~(WS_CAPTION | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU);
+		SetWindowLong(wHandle, GWL_STYLE, lStyle);
+
+		LONG lExStyle = GetWindowLong(wHandle, GWL_EXSTYLE);
+		lExStyle &= ~(WS_EX_DLGMODALFRAME | WS_EX_CLIENTEDGE | WS_EX_STATICEDGE);
+		SetWindowLong(wHandle, GWL_EXSTYLE, lExStyle);
+
+		ShowWindow(wHandle, SW_SHOW);
+		SetWindowPos(wHandle, NULL, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_NOOWNERZORDER);
+		
+		UpdateWindow(wHandle);
+
+		return TransformErrorCode::SUCCESS;
+	}
+	else
+	{
+		return TransformErrorCode::ERR_APP_FULLSCREEN;
+	}
+
+	return TransformErrorCode::ERR_UNKNOWN;
+}
+
+void Plugin_Init()
+{
+	PD2HOOK_LOG_LOG("Initializing ...");
+}
+
+void Plugin_Update()
+{
+	// Put your per-frame update code here
+
+	if (timer > timerTarget && !attemptTransform)
+	{
+		auto t = TransformWindow();
+		if (t != TransformErrorCode::SUCCESS)
+		{
+			// Failed? I need a output
+			ofstream f;
+			f.open("main_opt.log", ios::out);
+			f << "Process failed with error code: " << static_cast<int>(t) << endl;
+			f.close();
+
+			PD2HOOK_LOG_LOG("Process failed with error code: " + static_cast<int>(t));
+		}
+		else
+		{
+			ofstream f;
+			f.open("main_opt.log", ios::out);
+			f << "Process succeeded!" << endl;
+			f.close();
+			PD2HOOK_LOG_LOG("Transform into borderless succeeded.");
+		}
+
+		attemptTransform = true;
+	}
+
+	if (!attemptTransform)
+	{
+		timer++;
+	}
+}
+
+void Plugin_Setup_Lua(lua_State *L)
+{
+	// Deprecated, see this function's documentation (in superblt_flat.h) for more detail
+}
+
+int Plugin_PushLua(lua_State *L)
+{
+	return 0;
+}
